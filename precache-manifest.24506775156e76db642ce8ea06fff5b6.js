5eb6c0e7a8cbdd84d4526f152de701222dffe099
hardySin <deep16hardy@gmail.com>
2020-10-15 15:50:28 +0530

    Updates

---
+++ precache-manifest.24506775156e76db642ce8ea06fff5b6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7e83c673015f485e5fb5697b55c4042",
+    "url": "/movieFun/index.html"
+  },
+  {
+    "revision": "692de063f00001b3d856",
+    "url": "/movieFun/static/css/2.c3594674.chunk.css"
+  },
+  {
+    "revision": "488f44a24916514c5166",
+    "url": "/movieFun/static/css/main.06814edc.chunk.css"
+  },
+  {
+    "revision": "692de063f00001b3d856",
+    "url": "/movieFun/static/js/2.d3a1a79c.chunk.js"
+  },
+  {
+    "revision": "7a4e227475c0f933ffc114a43c163e6b",
+    "url": "/movieFun/static/js/2.d3a1a79c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "488f44a24916514c5166",
+    "url": "/movieFun/static/js/main.93ade471.chunk.js"
+  },
+  {
+    "revision": "18baeec6aa9e3691a1a5",
+    "url": "/movieFun/static/js/runtime-main.638a08f4.js"
+  }
+]);
