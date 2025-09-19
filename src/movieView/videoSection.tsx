ce42d6a5643557ce0a6cdeaf3fa5a295218c8e37
Hardy <deep16hardy@gmail.com>
2025-09-19 23:23:09 +0530

    code changes

--- src/movieView/videoSection.tsx
+++ src/movieView/videoSection.tsx
@@ -130,7 +130,7 @@ const YouTubeGallery: React.FC = () => {
                                 <div className="videos-grid">
                                     {section.videos.length > 0 ? "" :
                                         <div className="no-videos">
-                                            <p>No watch providers available.</p>
+                                            <p>No expandedSections[section.title]  available.</p>
                                         </div>}
                                     {section.videos.map(video => (
                                         <div key={video.id} className="video-card" onClick={() => openVideo(video)}>
